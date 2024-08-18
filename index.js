5fb48e856d78bbe07c3934beae0e849f83494697
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-08-18 16:16:05 -0300

    Part3:Ejercicios 3.13.-3.14.
    [X] Commit for the exercies from Ejercicios 3.13.-3.14.

--- index.js
+++ index.js
@@ -1,92 +1,72 @@
-  
-  const express = require('express')
-  const morgan = require('morgan')
-  
-  const cors = require('cors')
-  const app = express()
-  app.use(express.json())
-  app.use(cors())
-  
-  app.use(morgan('tiny'))
-  
-  morgan.token('body', (request) => JSON.stringify(request.body))
+const express = require('express')
+const morgan = require('morgan')
+const Person=require('./models/phonebook')
 
-  app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-  app.use(express.static('dist'))
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-  let phonebook=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-  ]
+app.use(morgan('tiny'))
+
+morgan.token('body', (request) => JSON.stringify(request.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
+
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(result => {
   
-  app.get('/api/persons', (request, response) => {
-    response.json(phonebook)
+    response.json(result)
   })
-  
-  
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = phonebook.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(note => {
+    response.json(note)
   })
-  
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    phonebook = phonebook.filter(person => person.id !== id)
-    response.status(204).end()
-  })
-  
-  
-  app.post('/api/persons', (request, response) => {
+})
+
+
+
+app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'content missing' 
+        return response
+            .status(400)
+            .json({error: 'content missing'})
+    }
+
+  
+    Person.find({name:body.name}).then(result => {
+        if(result.length>0){
+            return response
+            .status(400)
+            .json({error: 'name must be unique'})
+        }
+        const person=new Person({
+          name: body.name,
+          phone: body.number
+        })
+          person.save().then(() => {
+              response.json(person)
+          })
       })
-    }
-    if (phonebook.find(person => person.name === body.name)) {
-      return response.status(400).json({ 
-        error: 'name must be unique' 
-      })
-    }
-    const person = {
-      id: Math.floor(Math.random() * 10000),
-      name: body.name,
-      number: body.number
-    }
-    phonebook = phonebook.concat(person)
-    response.json(person)
-  })
-  
-  
-  app.get('/api/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${phonebook.length} people</p>
-    <p>${new Date()}</p>`)
-  })
-  
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
+    })
+    
+app.delete('/api/persons/:id', (request, response) => {
+   const phonebook = Person.findById(request.params.id)
+   phonebook.deleteOne().then((result) => {
+    response
+        .status(204)
+        .json(result)
+        .end()
+   })
+})
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
