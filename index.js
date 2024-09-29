5d8ec0e03396a57cf2dca24fa4d9b6c7b151ab0b
alejandrocabriales <alejandro.cabriales@dspot.com.pl>
2024-09-29 12:44:01 -0300

    test the github-actions

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(express.static('dist'))
 
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 app.get('/api/persons', (request, response) => {
